e568991e493cdda6c799c79fc3af8eba50d8759b
unknown <jatin.anand@contextbi.com>
2020-06-12 02:12:37 +0530

    functionality added

---
+++ Email Test App/Controllers/EmailController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Email_Test_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Email_Test_App.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Email model)
+        {
+            using (MailMessage mm = new MailMessage(model.Login, model.To))
+            {
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                if(model.CC != null)
+                {
+                   mm.CC.Add(model.CC);
+                }
+                if (model.BCC != null)
+                {
+                    mm.Bcc.Add(model.BCC);
+                }
+                mm.IsBodyHtml = false;
+                if (model.Attachment !=null)
+                {
+                    foreach (var file in model.Attachment)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        mm.Attachments.Add(new Attachment(file.OpenReadStream(), fileName));
+                    }
+                    
+                }
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = model.ServerAddress;
+                    smtp.EnableSsl = model.IsSSL;
+                    NetworkCredential NetworkCred = new NetworkCredential(model.Login, model.Password) ;
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = model.Port;
+                    smtp.Send(mm);
+                    ViewBag.Message = "Email sent.";
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Email Test App/Models/Email.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Email_Test_App.Models
+{
+    public class Email
+    {
+        [Required]
+        public string ServerAddress { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public bool IsSSL { get; set; }
+        [Required]
+        public int Port { get; set; }
+
+        [Required]
+        public string To { get; set; }
+        public string CC { get; set; }
+        public string BCC { get; set; }
+        public List<IFormFile> Attachment { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+    }
+}
